237b5e0eca1ec029cd94b1a55bb4f0cc03a03842
Daniel <strueber@mathematik.uni-marburg.de>
2020-12-03 19:44:03 +0100

    Fix loading of example rule set

--- ConsSustainingAnalysis/src/analysis/AnalysisDriver.java
+++ ConsSustainingAnalysis/src/analysis/AnalysisDriver.java
@@ -15,25 +15,22 @@ import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
 public class AnalysisDriver {
 	final static String INPUT_RULE_PATH = "cases/cragenerated";
 
-	private static Module defineInputRules(HenshinResourceSet resourceSet) {
+	private static List<Module> defineInputRules(HenshinResourceSet resourceSet) {
+		List<Module> result = new ArrayList<>();
 		if (INPUT_RULE_PATH.equals("cases/cramanual")) {
-			Module domainRules = resourceSet.getModule("mutation.henshin");
-			return domainRules;
+			Module m = resourceSet.getModule("mutation.henshin");
+			result.add(m);
 		}
 		if (INPUT_RULE_PATH.equals("cases/cragenerated")) {
-			Module module = HenshinPackage.eINSTANCE.getHenshinFactory().createModule();
 			for (File file : new File(INPUT_RULE_PATH).listFiles()) {
-				if (file.getName().endsWith(".henshin") &&
-						!file.getName().equals("checkrules.henshin")) {
+				if (file.getName().endsWith(".henshin") && !file.getName().equals("checkrules.henshin")) {
 					Module m = resourceSet.getModule(file.getName());
-					module.getUnits().addAll(m.getUnits());
+					result.add(m);
 				}
 			}
-			System.out.println(module.getUnits().size());
-			return module;
 
 		}
-		return null;
+		return result;
 	}
 
 	public static void main(String[] args) {
@@ -55,34 +52,36 @@ public class AnalysisDriver {
 		checkRules.add(checkPC3);
 		checkRules.add(checkPC3Prime);
 
-		Module domainRules = defineInputRules(resourceSet);
+		List<Module> modules = defineInputRules(resourceSet);
 
-		for (Rule domainRule : domainRules.getAllRules()) {
-			System.out.print(domainRule.getName() + " \t");
-			domainRule.getLhs().setFormula(null);
+		for (Module module : modules) {
+			for (Rule domainRule : module.getAllRules()) {
+				System.out.print(domainRule.getName() + " \t");
+				domainRule.getLhs().setFormula(null);
 
-			ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC1);
-			ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC2);
-			ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC3);
-			ConsSustainingAnalysis.printParIndependent(domainRule, checkPC2Prime);
-			ConsSustainingAnalysis.printParIndependent(domainRule, checkPC3Prime);
+				ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC1);
+				ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC2);
+				ConsSustainingAnalysis.printSeqIndependent(domainRule, checkPC3);
+				ConsSustainingAnalysis.printParIndependent(domainRule, checkPC2Prime);
+				ConsSustainingAnalysis.printParIndependent(domainRule, checkPC3Prime);
 
-			ConsSustainingAnalysis.printParDependent(domainRule, checkPC1);
-			ConsSustainingAnalysis.printParDependent(domainRule, checkPC2);
-			ConsSustainingAnalysis.printParDependent(domainRule, checkPC3);
-			ConsSustainingAnalysis.printSeqDependent(domainRule, checkPC2Prime);
-			ConsSustainingAnalysis.printSeqDependent(domainRule, checkPC3Prime);
+				ConsSustainingAnalysis.printParDependent(domainRule, checkPC1);
+				ConsSustainingAnalysis.printParDependent(domainRule, checkPC2);
+				ConsSustainingAnalysis.printParDependent(domainRule, checkPC3);
+				ConsSustainingAnalysis.printSeqDependent(domainRule, checkPC2Prime);
+				ConsSustainingAnalysis.printSeqDependent(domainRule, checkPC3Prime);
 
-			System.out.print("\t");
-			ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC1);
-			ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC2, checkPC2Prime);
-			ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC3, checkPC3Prime);
+				System.out.print("\t");
+				ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC1);
+				ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC2, checkPC2Prime);
+				ConsSustainingAnalysis.printConsistencySustaining(domainRule, checkPC3, checkPC3Prime);
 
-			System.out.print("\t");
-			ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC1);
-			ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC2, checkPC2Prime);
-			ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC3, checkPC3Prime);
-			System.out.println();
+				System.out.print("\t");
+				ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC1);
+				ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC2, checkPC2Prime);
+				ConsSustainingAnalysis.printConsistencyImproving(domainRule, checkPC3, checkPC3Prime);
+				System.out.println();
+			}
 		}
 	}
 
